c1bf053861979d420d119e3c4ea79c9228ca4e0c
Koleman Nix <kolemannix@gmail.com>
2014-01-02 19:11:24 -0600

    initial import

---
+++ elpa/gandalf-theme-0.1/gandalf-theme-autoloads.el
@@ -0,0 +1,26 @@
+;;; gandalf-theme-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil "gandalf-theme" "gandalf-theme.el" (21189 57916
+;;;;;;  0 0))
+;;; Generated autoloads from gandalf-theme.el
+
+(and load-file-name (boundp 'custom-theme-load-path) (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
+
+;;;***
+
+;;;### (autoloads nil nil ("gandalf-theme-pkg.el") (21189 57916 690847
+;;;;;;  0))
+
+;;;***
+
+(provide 'gandalf-theme-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; gandalf-theme-autoloads.el ends here
---
+++ user.el
@@ -0,0 +1,1 @@
+(setq ns-right-alternate-modifier nil)
