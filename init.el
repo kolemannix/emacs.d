4058a36ce219449c9e0694f91a87523eda1abed6
Koleman Nix <kolemannix@gmail.com>
2014-02-04 14:35:43 -0500

    make config more modular
    Signed-off-by: Koleman Nix <kolemannix@gmail.com>

--- init.el
+++ init.el
@@ -4,7 +4,7 @@
 (add-to-list 'package-archives
 	     '("marmalade" . "http://marmalade-repo.org/packages/"))
 
-(add-to-list 'load-path "~/.emacs.d/extras/")
+(add-to-list 'load-path "~/.emacs.d/knix/")
 (package-initialize)
 
 (defun enable-my-elisp-settings ()
@@ -25,11 +25,10 @@
 (require 'key-chord)
 (key-chord-mode 1)
 (require 'auto-complete)
-(require 'paredit)
 (require 'mic-paren)
 (paren-activate)
 
-(load-file "~/.emacs.d/extras/smex-config.el")
+(require 'knix-smex)
 
 ;; Rainbow delimiters in all programming-related files
 (require 'rainbow-delimiters)
@@ -41,44 +40,28 @@
 
 ;; ------------------- Settings -------------------- ;;
 
-(require 'ido)
-(ido-mode t)
-(setq ido-enable-flex-matching t)
+(require 'knix-ido)
 
 (key-chord-define evil-normal-state-map "ef" 'eval-defun)
 
 ; No splash screen
 (setq inhibit-startup-message t)
 
-
 ;; Load mah theme
 (load-theme 'solarized-dark t)
 
-
-;; Line numbers, with a space for padding
-;; (global-linum-mode t)
-;; (setq linum-format "%d ")
-
-; Scrolling behavior
+;; Scrolling behavior
 (setq redisplay-dont-pause t
       scroll-margin 7
       scroll-step 1
       scroll-conservatively 10000
       scroll-preserve-screen-position 1)
 
-;; (setq max-specpdl-size 10000) 
-;; (setq max-lisp-eval-depth 10000)
-
-;; Font lock mode
-(if (fboundp 'global-font-lock-mode)
-    (global-font-lock-mode 1)        ; GNU Emacs
-  (setq font-lock-auto-fontify t))   ; XEmacs
-
-					; Backups to home folder, no autosave
+;; Backups to home folder, no autosave
 (setq backup-directory-alist `(("." . "~/.backup")))
 (setq auto-save-default nil)
 
-;; ----------------- Copy / Paste -------------------- ;;
+;; ----------------- Copy / Paste in OSX -------------------- ;;
 (defun copy-from-osx ()
   (shell-command-to-string "pbpaste"))
 
@@ -91,17 +74,21 @@
 (setq interprogram-cut-function 'paste-to-osx)
 (setq interprogram-paste-function 'copy-from-osx)
 
-;; ----------------- Key bindings -------------------- ;;
+;; ----------------- Core bindings -------------------- ;;
 (define-key evil-insert-state-map (kbd "C-s") (lambda () (interactive) (save-buffer) (evil-normal-state)))
 (define-key evil-normal-state-map (kbd "C-s") 'save-buffer)
 (define-key evil-insert-state-map (kbd "RET") 'evil-ret-and-indent)
 ;; Map jk -> ESC 
 (key-chord-define evil-insert-state-map "jk" 'evil-normal-state)
 
-
 ;; Meta key
-(global-set-key (kbd "M-x") 'smex)
 (setq ns-right-alternate-modifier nil)
+
+(global-auto-complete-mode t)
+(define-key ac-completing-map (kbd "C-n") 'ac-next)
+(define-key ac-completing-map (kbd "C-p") 'ac-previous)
+(define-key ac-completing-map "\t" 'ac-complete)
+
 
 ;; LaTeX
 (require 'smartparens-config)
@@ -119,51 +106,8 @@
 
 (add-hook 'LaTeX-mode-hook 'ac-LaTeX-mode-setup)
 
-;; Clojure
-; Cider preferences
-(setq package-archive-exclude-alist '("marmalade" . cider))
-(load-file "~/.emacs.d/extras/paredit-config.el")
+(require 'knix-paredit)
 
-;; (defun my-cider-start () "Jacks in to a REPL and loads the current buffer" 
-;;   (interactive)
-;;   (cider-jack-in)
-;;   (cider-load-current-buffer))
-;; Unfortunately, the above function doesn't work because it doesn't wait for the REPL to finish launching.
+(require 'knix-clojure)
 
-(define-key evil-normal-state-map (kbd "<SPC>") 'cider-eval-expression-at-point)
-;; (define-key evil-normal-state-map (kbd "C-c j") 'my-cider-start)
-(define-key evil-normal-state-map (kbd "C-c j") 'cider-jack-in)
-(define-key evil-normal-state-map (kbd "C-c b") 'cider-jump)
-(define-key evil-normal-state-map (kbd "C-c l") 'cider-load-current-buffer)
-
-(setq nrepl-hide-special-buffers t)
-(setq cider-repl-pop-to-buffer-on-connect nil)
-;; (setq cider-popup-stacktraces nil)
-
-; Cider hooks
-(add-hook 'cider-mode-hook 'cider-turn-on-eldoc-mode)
-
-;; highlight expression on eval
-(require 'highlight)
-(require 'cider-eval-sexp-fu)
-(setq cider-eval-sexp-fu-flash-duration 0.2)
-
-;; Set up Clojure auto-complete
-(require 'ac-nrepl)
-(add-hook 'cider-repl-mode-hook 'ac-nrepl-setup)
-(add-hook 'cider-mode-hook 'ac-nrepl-setup)
-(eval-after-load "auto-complete"
-  '(add-to-list 'ac-modes 'cider-repl-mode))
-(eval-after-load "cider"
-  '(define-key cider-mode-map (kbd "C-c C-d") 'ac-nrepl-popup-doc))
-(eval-after-load "cider"
-  '(define-key cider-mode-map (kbd "C-c b") 'cider-jump))
-(eval-after-load "cider"
-  '(define-key cider-mode-map (kbd "C-c q") 'ac-nrepl-popup-doc))
-
-(print "hello")
-
-(global-auto-complete-mode t)
-(define-key ac-completing-map (kbd "C-n") 'ac-next)
-(define-key ac-completing-map (kbd "C-p") 'ac-previous)
-(define-key ac-completing-map "\t" 'ac-complete)
+(require 'knix-ac)
